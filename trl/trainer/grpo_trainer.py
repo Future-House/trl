3bf982ca0204d815fb03f45e64bd2f304ae58e0d
Siddharth M Narayanan <sid@futurehouse.org>
2025-02-02 16:58:25 -0600

    add more logs

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -39,7 +39,7 @@ from transformers import (
     is_wandb_available,
 )
 from transformers.integrations.deepspeed import is_deepspeed_zero3_enabled
-from transformers.utils import is_peft_available, logging
+from transformers.utils import is_peft_available
 
 from ..data_utils import apply_chat_template, is_conversational, maybe_apply_chat_template
 from ..import_utils import is_vllm_available
@@ -57,9 +57,6 @@ if is_vllm_available():
 
 if is_wandb_available():
     import wandb
-
-
-logger = logging.get_logger("GRPOTrainer")
 
 # What we call a reward function is a callable that takes a list of prompts and completions and returns a list of
 # rewards. When it's a string, it's a model ID, so it's loaded as a pretrained model.
@@ -406,7 +403,6 @@ class GRPOTrainer(Trainer):
             prompt_ids = prompt_ids[:, -self.max_prompt_length :]
             prompt_mask = prompt_mask[:, -self.max_prompt_length :]
 
-        logger.info("Starting generation")
         # Generate completions using either vLLM or regular generation
         if self.args.use_vllm:
             # First, have main process load weights if needed
